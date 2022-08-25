1e07652ecdaae1aa8de98e498888a20c53b7e181
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-26 01:06:35 +0500

    Ex52 solved

--- HomeWork_26_08_22/Ex52_medium_column/Program.cs
+++ HomeWork_26_08_22/Ex52_medium_column/Program.cs
@@ -1,8 +1,7 @@
 ﻿// Ex52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 //  1 4 7 2
 //  5 9 2 3
-//  8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+//  Среднее арифметическое каждого столбца: 3; 6,5; 4,5; 2,5.
 
 void FillArray(int[,] matr)
 {
@@ -16,20 +15,18 @@ void FillArray(int[,] matr)
         Console.WriteLine();
     }
 }
-
-void MediumColumn(int[,] matr)
+void MediumColumn(int[,] matr)    //считает среднее арифметическое столбцов
 {
     for (int j = 0; j < matr.GetLength(1); j++)
         {
-            double average = 0;
+            double average = 0.0;
             for (int i = 0; i < matr.GetLength(0); i++)
             {
-                average = average + matr[i, j];   
+                average += matr[i, j];   
             }
+            Console.Write($" {average / matr.GetLength(0)} ");
         }
-        Console.Write($" {average / matr.GetLength(0)} ");
 }
-
 Console.Write("Введите размер матрицы. Укажите количество строк m = ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("  и столбцов n = ");
