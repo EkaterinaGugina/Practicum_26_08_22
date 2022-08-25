a281f88f4c846a61b312043d40c086200e8fc219
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-25 08:27:26 +0500

    commit and add folder Practicum 26_08

---
+++ HomeWork_26_08_22/Ex47_fill_array/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ex47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = Math.Round(new Random().Next(-10,10), 1);    
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите размер матрицы. Укажите количество строк m = ");
+int m = Convert.ToInt32(Console.Read());
+Console.Write("  и столбцов n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, m];
+FillArray(matrix);
+
